104b49a2ec1bcfc43464a7100d244afc8a370607
plurr1k <plurrik666@list.ru>
2025-04-16 16:38:41 +0300

    Fix working plugin after translating

--- ufit/base/__init__.py
+++ ufit/base/__init__.py
@@ -2,9 +2,13 @@ from .src.preferences import preferences
 from .src.properties import properties
 from .src.operators import operators
 from .src.ui import ui
+import bpy
 
 
 def register():
+    global user_use_translate_new_dataname
+    user_use_translate_new_dataname = bpy.context.preferences.view.use_translate_new_dataname
+    bpy.context.preferences.view.use_translate_new_dataname = False
     preferences.register()
     properties.register()
     operators.register()
@@ -12,6 +16,8 @@ def register():
 
 
 def unregister():
+    global user_use_translate_new_dataname
+    bpy.context.preferences.view.use_translate_new_dataname = user_use_translate_new_dataname
     preferences.unregister()
     properties.unregister()
     operators.unregister()
