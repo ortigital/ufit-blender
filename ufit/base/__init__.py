280a520c3183bd9063483d10d25306b697c21106
plurr1k <plurrik666@list.ru>
2025-04-18 16:01:28 +0300

    Fix translate errors and update readme

--- ufit/base/__init__.py
+++ ufit/base/__init__.py
@@ -3,11 +3,10 @@ from .src.properties import properties
 from .src.operators import operators
 from .src.ui import ui
 import bpy
-
+from .storage import Storage
 
 def register():
-    global user_use_translate_new_dataname
-    user_use_translate_new_dataname = bpy.context.preferences.view.use_translate_new_dataname
+    Storage.user_use_translate_new_dataname = bpy.context.preferences.view.use_translate_new_dataname
     bpy.context.preferences.view.use_translate_new_dataname = False
     preferences.register()
     properties.register()
@@ -16,8 +15,7 @@ def register():
 
 
 def unregister():
-    global user_use_translate_new_dataname
-    bpy.context.preferences.view.use_translate_new_dataname = user_use_translate_new_dataname
+    bpy.context.preferences.view.use_translate_new_dataname = Storage.user_use_translate_new_dataname
     preferences.unregister()
     properties.unregister()
     operators.unregister()
---
+++ ufit/base/storage.py
@@ -0,0 +1,4 @@
+import bpy
+
+class Storage:
+    user_use_translate_new_dataname = bpy.context.preferences.view.use_translate_new_dataname
